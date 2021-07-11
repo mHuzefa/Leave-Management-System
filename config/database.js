6ffc42f3d41c585c856703ccbc1660e45ede19a6
unknown <mhuzefah1@gmail.com>
2021-07-11 17:15:12 +0500

    Leave Management System

---
+++ config/database.js
@@ -0,0 +1,7 @@
+(function(){
+
+    var dbObj = {};
+    dbObj.url = 'mongodb+srv://admin:admin@cluster0.snuuc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+    module.exports = dbObj;
+})();
